33255e31e287bcc32a5d37c90f7e705244aa6c6b
naomiwix <82802093+naomiwix@users.noreply.github.com>
2023-03-14 09:29:40 +0200

    [Person-Segmentation]first commit #pr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@
 
 import React, { useRef } from "react";
 // import logo from './logo.svg';
-import * as tf from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs"; 
 import * as bodyPix from "@tensorflow-models/body-pix";
 import Webcam from "react-webcam";
 import "./App.css";
